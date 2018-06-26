68adf691a9444cf7c0a58537c6a854a313a38ed6
kevin <kevinelsen@hotmail.fr>
2018-06-26 10:59:29 +0200

    exo3

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*Déclaration d'une fonction.*/
+function over1() {
+document.getElementById('text').style.color ='green';
+}
+/*Déclaration d'une fonction.*/
+function over2() {
+document.getElementById('text').style.color ='red';
+}
+/*Déclaration d'une fonction.*/
+function over3() {
+document.getElementById('text').style.color ='blue';
+}
